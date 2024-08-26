03f1fe291337d3b99ab6fdb82b04fe5d92177ce7
Daniel J. Lauk <daniel.lauk@bbbaden.ch>
2024-08-26 22:09:34 +0200

    build(eslint): ignore unused function parameters

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,12 @@ import prettier from "eslint-config-prettier"
 import globals from "globals"
 
 export default [
-	{ languageOptions: { globals: { ...globals.node } } },
+	{
+		languageOptions: {
+			globals: { ...globals.node },
+		},
+		rules: { "no-unused-vars": ["error", { argsIgnorePattern: "^_" }] },
+	},
 	eslint.configs.recommended,
 	prettier,
 ]
